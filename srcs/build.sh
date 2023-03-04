a052f853c01c0e1f84943398021315cf586af727
fdeĉ <fd@42l.fr>
2023-03-04 23:20:56 +0100

    update build script

--- srcs/build.sh
+++ srcs/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-ocamlc -c types.ml
+#ocamlc -c types.ml
+#menhir polynomial_parser.mly --infer
+#ocamlc -c polynomial_parser.mli
+#ocamllex polynomial_lexer.mll 
+#ocamlc -c polynomial_lexer.ml;
+#ocamlc -c polynomial_parser.ml;
+#ocamlc -c main.ml
+#ocamlc -o polynomial_resolver polynomial_lexer.cmo polynomial_parser.cmo main.cmo
+
+ocamlopt -c types.ml
 menhir polynomial_parser.mly --infer
-ocamlc -c polynomial_parser.mli
+ocamlopt -c polynomial_parser.mli
 ocamllex polynomial_lexer.mll 
-ocamlc -c polynomial_lexer.ml;
-ocamlc -c polynomial_parser.ml;
-ocamlc -c main.ml
-ocamlc -o polynomial_resolver polynomial_lexer.cmo polynomial_parser.cmo main.cmo
+ocamlopt -c polynomial_lexer.ml;
+ocamlopt -c polynomial_parser.ml;
+ocamlopt -c main.ml
+ocamlopt -o polynomial_resolver polynomial_lexer.cmx polynomial_parser.cmx main.cmx types.cmx
